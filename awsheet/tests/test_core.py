665bb93304fd620cc13e775d786400d37c604250
JLight <j.gareth.light@gmail.com>
2014-12-08 17:35:38 -0500

    remove weird formatting a_ws_heet

--- awsheet/tests/test_core.py
+++ awsheet/tests/test_core.py
@@ -2,82 +2,82 @@ import unittest
 
 class TestAWSHeet(unittest.TestCase):
     def test___init__(self):
-        # a_ws_heet = AWSHeet(defaults, name)
+        # awsheet = AWSHeet(defaults, name)
         assert False # TODO: implement your test here
 
     def test_add_dependent_resource(self):
-        # a_ws_heet = AWSHeet(defaults, name)
-        # self.assertEqual(expected, a_ws_heet.add_dependent_resource(dependent_resource, key_name))
+        # awsheet = AWSHeet(defaults, name)
+        # self.assertEqual(expected, awsheet.add_dependent_resource(dependent_resource, key_name))
         assert False # TODO: implement your test here
 
     def test_add_instance_to_elb(self):
-        # a_ws_heet = AWSHeet(defaults, name)
-        # self.assertEqual(expected, a_ws_heet.add_instance_to_elb(defaults, elb_name, instance_helper))
+        # awsheet = AWSHeet(defaults, name)
+        # self.assertEqual(expected, awsheet.add_instance_to_elb(defaults, elb_name, instance_helper))
         assert False # TODO: implement your test here
 
     def test_add_resource(self):
-        # a_ws_heet = AWSHeet(defaults, name)
-        # self.assertEqual(expected, a_ws_heet.add_resource(resource))
+        # awsheet = AWSHeet(defaults, name)
+        # self.assertEqual(expected, awsheet.add_resource(resource))
         assert False # TODO: implement your test here
 
     def test_add_resource_ref(self):
-        # a_ws_heet = AWSHeet(defaults, name)
-        # self.assertEqual(expected, a_ws_heet.add_resource_ref(resource, resource_ref_key))
+        # awsheet = AWSHeet(defaults, name)
+        # self.assertEqual(expected, awsheet.add_resource_ref(resource, resource_ref_key))
         assert False # TODO: implement your test here
 
     def test_exec_awscli(self):
-        # a_ws_heet = AWSHeet(defaults, name)
-        # self.assertEqual(expected, a_ws_heet.exec_awscli(cmd))
+        # awsheet = AWSHeet(defaults, name)
+        # self.assertEqual(expected, awsheet.exec_awscli(cmd))
         assert False # TODO: implement your test here
 
     def test_get_destroy(self):
-        # a_ws_heet = AWSHeet(defaults, name)
-        # self.assertEqual(expected, a_ws_heet.get_destroy())
+        # awsheet = AWSHeet(defaults, name)
+        # self.assertEqual(expected, awsheet.get_destroy())
         assert False # TODO: implement your test here
 
     def test_get_environment(self):
-        # a_ws_heet = AWSHeet(defaults, name)
-        # self.assertEqual(expected, a_ws_heet.get_environment())
+        # awsheet = AWSHeet(defaults, name)
+        # self.assertEqual(expected, awsheet.get_environment())
         assert False # TODO: implement your test here
 
     def test_get_project(self):
-        # a_ws_heet = AWSHeet(defaults, name)
-        # self.assertEqual(expected, a_ws_heet.get_project())
+        # awsheet = AWSHeet(defaults, name)
+        # self.assertEqual(expected, awsheet.get_project())
         assert False # TODO: implement your test here
 
     def test_get_region(self):
-        # a_ws_heet = AWSHeet(defaults, name)
-        # self.assertEqual(expected, a_ws_heet.get_region())
+        # awsheet = AWSHeet(defaults, name)
+        # self.assertEqual(expected, awsheet.get_region())
         assert False # TODO: implement your test here
 
     def test_get_value(self):
-        # a_ws_heet = AWSHeet(defaults, name)
-        # self.assertEqual(expected, a_ws_heet.get_value(name, kwargs, default, required))
+        # awsheet = AWSHeet(defaults, name)
+        # self.assertEqual(expected, awsheet.get_value(name, kwargs, default, required))
         assert False # TODO: implement your test here
 
     def test_get_version(self):
-        # a_ws_heet = AWSHeet(defaults, name)
-        # self.assertEqual(expected, a_ws_heet.get_version())
+        # awsheet = AWSHeet(defaults, name)
+        # self.assertEqual(expected, awsheet.get_version())
         assert False # TODO: implement your test here
 
     def test_is_resource_ref(self):
-        # a_ws_heet = AWSHeet(defaults, name)
-        # self.assertEqual(expected, a_ws_heet.is_resource_ref(ref_str))
+        # awsheet = AWSHeet(defaults, name)
+        # self.assertEqual(expected, awsheet.is_resource_ref(ref_str))
         assert False # TODO: implement your test here
 
     def test_load_creds(self):
-        # a_ws_heet = AWSHeet(defaults, name)
-        # self.assertEqual(expected, a_ws_heet.load_creds())
+        # awsheet = AWSHeet(defaults, name)
+        # self.assertEqual(expected, awsheet.load_creds())
         assert False # TODO: implement your test here
 
     def test_parse_args(self):
-        # a_ws_heet = AWSHeet(defaults, name)
-        # self.assertEqual(expected, a_ws_heet.parse_args())
+        # awsheet = AWSHeet(defaults, name)
+        # self.assertEqual(expected, awsheet.parse_args())
         assert False # TODO: implement your test here
 
     def test_parse_creds_from_file(self):
-        # a_ws_heet = AWSHeet(defaults, name)
-        # self.assertEqual(expected, a_ws_heet.parse_creds_from_file(filename))
+        # awsheet = AWSHeet(defaults, name)
+        # self.assertEqual(expected, awsheet.parse_creds_from_file(filename))
         assert False # TODO: implement your test here
 
 if __name__ == '__main__':
