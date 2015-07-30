0a672c526798ded543d15983e1908405bd26bd8b
JLight <j.gareth.light@gmail.com>
2015-07-30 15:16:50 -0400

    only converge and destroy are required to be implemented

--- awsheet/helpers/awshelper.py
+++ awsheet/helpers/awshelper.py
@@ -23,29 +23,26 @@ class AWSHelper(object):
     def __str__(self):
         return str(type(self))
 
+
     #- these methods form the Heet Protocol
-    #- All helper obejcts must support all of these methods.
-    @abstractmethod
     def post_init_hook(self):
         self.heet.logger.debug("no defined method post_init_hook for %s" % type(self))
 
-    @abstractmethod
     def post_converge_hook(self):
         self.heet.logger.debug("no defined method post_converge_hook for %s" % type(self))
 
-    @abstractmethod
     def pre_destroy_hook(self):
         self.heet.logger.debug("no defined method pre_destroy_hook for %s" % type(self))
 
-    @abstractmethod
     def get_cname_target(self):
         raise Exception("no cname target defined for %s" % self)
 
-    @abstractmethod
+    #- these two methods are required to implemented by the subclass
+    @abc.abstractmethod
     def converge(self):
         raise Exception("Heet Protocol Violation: unimplemented converge method")
 
-    @abstractmethod
+    @abc.abstractmethod
     def destroy(self):
         raise Exception("Heet Protocol Violation: unimplemented destroy method")
 
